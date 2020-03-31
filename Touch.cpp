c2ae0fe7a8a98c2269a2cfce1bae213cac18f4d2
Daisuke Sato <dsato@jp.ibm.com>
2020-03-31 18:30:14 -0400

    add Touch

---
+++ Touch.cpp
@@ -0,0 +1,32 @@
+#include "Touch.h"
+
+Touch::Touch()
+        : SensorReader("touch", &currTouched)
+{}
+
+bool Touch::init(){
+    // Default address is 0x5A, if tied to 3.3V its 0x5B
+    // If tied to SDA its 0x5C and if SCL then 0x5D
+
+    //first check if MPR121 is plugged in
+    Wire.beginTransmission(0x5A);
+    uint8_t error = Wire.endTransmission();
+
+    if (error == 0){ //success, initialize MPR121
+        cap.begin(0x5A);
+        return true;
+    }
+    else{  //unknown error, return failure
+        return false;
+    }
+}
+
+void Touch::publish(ros::NodeHandle &nh){
+    currTouched.data = touchData;
+    this->pub.publish( &currTouched );
+}
+
+bool Touch::getTouched(int pinNum){
+    touchData = cap.touched();
+    return (touchData >> pinNum) & 0x1;
+}
