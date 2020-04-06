6ed2b3faee2e45641439877499c6436fbcc11c92
Arun Kumar <arun89@terpmail.umd.edu>
2020-04-06 13:17:10 -0400

    Modified touch velocity

--- Velocity.h
+++ Velocity.h
@@ -1,7 +1,7 @@
 #ifndef ARDUINO_NODE_VELOCITY_H
 #define ARDUINO_NODE_VELOCITY_H
 
-#include <geometry_msgs/Twist.h>
+#include <std_msgs/Float32.h>
 #include "SensorReader.h"
 
 class Velocity : public SensorReader {
@@ -11,7 +11,7 @@ class Velocity : public SensorReader {
   void update(bool status);
   void publish(ros::NodeHandle &nh);
  private:
-  geometry_msgs::Twist vel_msg;
+  std_msgs::Float32 vel_msg;
 };
 
 #endif // ARDUINO_NODE_VELOCITY_H
