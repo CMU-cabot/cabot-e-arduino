6e2800675134bcb303cc893aa86a392ad7b54151
Arun Kumar <arun89@terpmail.umd.edu>
2020-04-06 13:17:10 -0400

    Modified touch velocity

--- Velocity.cpp
+++ Velocity.cpp
@@ -1,25 +1,11 @@
 #include "Velocity.h"
 
 Velocity::Velocity()
-        : SensorReader("velocity", &vel_msg)
+        : SensorReader("cabot/touch_speed", &vel_msg)
 {}
 
 void Velocity::update(bool status) {
-  if (status) {
-    vel_msg.linear.x = 20.0;
-    vel_msg.linear.y = 0.0;
-    vel_msg.linear.z = 0.0;
-    vel_msg.angular.x = 0.0;
-    vel_msg.angular.y = 0.0;
-    vel_msg.angular.z = 20.0;
-  } else {
-    vel_msg.linear.x = 0.0;
-    vel_msg.linear.y = 0.0;
-    vel_msg.linear.z = 0.0;
-    vel_msg.angular.x = 0.0;
-    vel_msg.angular.y = 0.0;
-    vel_msg.angular.z = 0.0;
-  }
+  status ? vel_msg.data = 2.0 : vel_msg.data = 0.0;;
 }
 
 void Velocity::publish(ros::NodeHandle &nh){
